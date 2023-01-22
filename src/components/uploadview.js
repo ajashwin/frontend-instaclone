bc57dadaebe3c3f24fbcaf92cc1fc61d477ed9d2
Ashwin Jose <115637412+ajashwin@users.noreply.github.com>
2023-01-22 19:10:08 +0530

    Update uploadview.js

--- src/components/uploadview.js
+++ src/components/uploadview.js
@@ -13,7 +13,7 @@ export default function UploadView({ setIndicators }) {
         let dataFromForm = Object.fromEntries(formData.entries());
         console.log(dataFromForm);
 
-        await fetch("http://localhost:3600/posts", {
+        await fetch("https://instaclone-backend-1mz8.onrender.com/posts", {
             method: "post",
             body: formData,
             redirect: "follow"
