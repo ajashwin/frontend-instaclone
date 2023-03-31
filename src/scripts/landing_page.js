d2c1178c0bfd7d37ac1fa6dadc2670c504e1a218
Ashwin Jose <ajashwin8880@gmail.com>
2023-03-31 12:14:41 +0530

    made changes

--- src/scripts/landing_page.js
+++ src/scripts/landing_page.js
@@ -7,12 +7,10 @@ export default function Landing_page() {
   <div class="container">
   {/* <h2>Landing Page</h2> */}
   <section class="left">
-  <img class="landing_page_image" src="images/landingpage.jpg" alt="landing_pages"></img>
+  <img class="landing_page_image" src="images/landingPage.png" alt="landing_pages"></img>
   </section>
   <section class="right">
-    <h1 class="text">10x InstaClone by Ashwin</h1>
-    <br> 
-    <br>
+    <h1 class="text">InstaClone by Ashwin</h1> <br/>
     <Link to="/postview"><button> Enter </button></Link>
   </section>
   </div>
